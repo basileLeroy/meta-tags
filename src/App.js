9ef81c0539a83a04b4a11d3e2a99af99275e5f4f
Basile <basile.leroy.jnm@hotmail.com>
2021-09-21 10:52:57 +0200

    Changing naming in image tag

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import image from './meta_card.png'
+import myLogo from './meta_card.png'
 
 
 function App() {
   return (
       <div className="card position-absolute top-50 start-50 translate-middle" style={{width: 18 + "rem"}}>
-          <img src={image} className="card-img-top" alt="my image" />
+          <img src={myLogo} className="card-img-top" alt="my logo" />
               <div className="card-body">
                   <h5 className="card-title">Hello Verou</h5>
                   <p className="card-text">A quick introduction to using Meta tags, more precisely Open Graph tags (OG).</p>
